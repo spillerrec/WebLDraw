54314e0f78abf420d46b1a06e146145e4c09ed64
spillerrec <spillerrec@hotmail.com>
2014-03-12 23:52:23 +0100

    IMPROVE: more efficient quad rendering

--- Viewer/web/webgl.dart
+++ Viewer/web/webgl.dart
@@ -148,10 +148,12 @@ class Canvas{
                                                      x1, y1, z1,
                                                      x2, y2, z2,
                                                      x3, y3, z3,
-                                                     x4, y4, z4,
+                                                     x4, y4, z4
                                                      ]);
-    draw_triangle(x1, y1, z1, x2, y2, z2, x3, y3, z3);
-    draw_triangle(x1, y1, z1, x4, y4, z4, x3, y3, z3);
+
+    gl.bufferDataTyped( ARRAY_BUFFER, vertices, STATIC_DRAW );
+    gl.vertexAttribPointer( aVertexPosition, 3, FLOAT, false, 0, 0 );
+    gl.drawArrays( TRIANGLE_FAN, 0, 4 );
   }
   
 
